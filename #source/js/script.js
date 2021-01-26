4a005164ea6696c40d435da22ed189a0121957cd
ruvick <rudikov.web@ya.ru>
2021-01-27 01:32:04 +0300

    Улучшения и Дополнения

--- #source/js/script.js
+++ #source/js/script.js
@@ -16,8 +16,10 @@ if (isMobile.iOS()) {
 	var act = "touchstart";
 }
 
+
+
 //BURGER
-let iconMenu = document.querySelector(".icon-menu");
+let iconMenu = document.querySelector(".icon-menu"); 
 let body = document.querySelector("body");
 let menuBody = document.querySelector(".menu__body");
 if (iconMenu) {
@@ -27,6 +29,52 @@ if (iconMenu) {
 		menuBody.classList.toggle("active");
 	});
 }
+
+
+
+// Маска телефона
+var inputmask_phone = {"mask": "+9(999)999-99-99"};
+jQuery("input[type=tel]").inputmask(inputmask_phone); 
+
+
+
+//Валидация телефона + Отправщик
+jQuery('.header__form button').click(function(e){ 
+        e.preventDefault();
+        
+        let persPhone = jQuery('.header__form input[name=tel]').val(); 
+        if ((persPhone == "")||(persPhone.indexOf("_")>0)) { 
+            $(this).siblings('input[name=tel]').css("background-color","#ff91a4")
+            return;
+        }
+        
+        var  jqXHR = jQuery.post(
+          "../sender/send.php",
+            {
+                phone: jQuery('.header__form input[name=tel]').val(),    
+                name: jQuery('.header__form input[name=name]').val(),
+                mail: jQuery('.header__form textarea[name=text]').val(),
+            }
+            
+        );
+        
+        
+        jqXHR.done(function (responce) {
+            console.log(responce);
+            document.location.href = "../thank-you.html"; 
+            jQuery('.header__form input[name=tel]').val("");  
+            jQuery('.header__form input[name=name]').val("");
+            jQuery('.header__form textarea[name=text]').val("");
+        });
+        
+        jqXHR.fail(function (responce) {
+            console.log(responce);
+            alert("Произошла ошибка попробуйте позднее!");
+        });
+ 
+    });
+
+
 
 //ZOOM
 if ($('.gallery').length > 0) {
