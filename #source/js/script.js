82e6aa10ad54042231b668f522fa83c6df70e25c
ruvick <rudikov.web@ya.ru>
2021-01-31 02:31:52 +0300

    Правки мобильного меню

--- #source/js/script.js
+++ #source/js/script.js
@@ -21,7 +21,7 @@ if (isMobile.iOS()) {
 //BURGER
 let iconMenu = document.querySelector(".icon-menu"); 
 let body = document.querySelector("body");
-let menuBody = document.querySelector(".menu__body");
+let menuBody = document.querySelector(".mob-menu");
 if (iconMenu) {
 	iconMenu.addEventListener("click", function () {
 		iconMenu.classList.toggle("active");
