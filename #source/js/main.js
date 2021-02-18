042685e55978b00df431643f2d12c9a58f0c27ba
ruvick <rudikov.web@ya.ru>
2021-02-18 21:21:51 +0300

    Дополнения

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,5 +1,7 @@
 //@prepros-append jq-start.js
 //@prepros-append forms.js
+//@prepros-append slick.min.js
+//@prepros-append jquery.fancybox.min.js
 //@prepros-append jquery.inputmask.bundle.min.js
 //@prepros-append script.js
-//@prepros-append jq-end.js 
+//@prepros-append jq-end.js  
