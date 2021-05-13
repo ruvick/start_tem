84d12043a8db5d48ed3fec0fcf06cf85f6bd87e5
Ruvick <rudikov.web@ya.ru>
2021-05-14 00:41:57 +0300

    Добавил файлы корзины

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,24 +1,24 @@
 var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
 if (isMobile.any()) { }
 
-	if (location.hash) {
-		var hsh = location.hash.replace('#', '');
-		if ($('.popup-' + hsh).length > 0) {
-			popupOpen(hsh);
-		} else if ($('div.' + hsh).length > 0) {
-			$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
-		}
+if (location.hash) {
+	var hsh = location.hash.replace('#', '');
+	if ($('.popup-' + hsh).length > 0) {
+		popupOpen(hsh);
+	} else if ($('div.' + hsh).length > 0) {
+		$('body,html').animate({ scrollTop: $('div.' + hsh).offset().top, }, 500, function () { });
 	}
-	$('.wrapper').addClass('loaded');
+}
+$('.wrapper').addClass('loaded');
 
-	var act = "click";
-	if (isMobile.iOS()) {
-		var act = "touchstart"; 
-	}
+var act = "click";
+if (isMobile.iOS()) {
+	var act = "touchstart";
+}
 
 
 //BURGER
-let iconMenu = document.querySelector(".icon-menu"); 
+let iconMenu = document.querySelector(".icon-menu");
 let body = document.querySelector("body");
 let menuBody = document.querySelector(".mob-menu");
 if (iconMenu) {
@@ -30,14 +30,14 @@ if (iconMenu) {
 }
 
 // Открытие ПК меню при наведении до 1024px
-if (document.body.clientWidth>1024){
+if (document.body.clientWidth > 1024) {
 	function hideMenu() {
 		$('.mob-menu').slideUp(600);
 	}
 	function showMenu() {
 		$('.mob-menu').slideDown(600);
 	}
-	$(document).ready(function() {
+	$(document).ready(function () {
 		$(".menu__catalogy").on("mouseover", showMenu);
 		$(".header__menu").on("mouseleave", hideMenu);
 	});
@@ -113,36 +113,36 @@ $('.plus').click(function () {
 
 
 // Маска телефона
-var inputmask_phone = {"mask": "+9(999)999-99-99"};
-jQuery("input[type=tel]").inputmask(inputmask_phone);  
+var inputmask_phone = { "mask": "+9(999)999-99-99" };
+jQuery("input[type=tel]").inputmask(inputmask_phone);
 
 
 
 //Валидация телефона + Отправщик
-jQuery('.header__form button').click(function(e){ 
+jQuery('.header__form button').click(function (e) {
 	e.preventDefault();
 
-	let persPhone = jQuery('.header__form input[name=tel]').val(); 
-	if ((persPhone == "")||(persPhone.indexOf("_")>0)) { 
-		$(this).siblings('input[name=tel]').css("background-color","#ff91a4")
+	let persPhone = jQuery('.header__form input[name=tel]').val();
+	if ((persPhone == "") || (persPhone.indexOf("_") > 0)) {
+		$(this).siblings('input[name=tel]').css("background-color", "#ff91a4")
 		return;
 	}
 
-	var  jqXHR = jQuery.post(
+	var jqXHR = jQuery.post(
 		"../sender/send.php",
 		{
-			phone: jQuery('.header__form input[name=tel]').val(),    
+			phone: jQuery('.header__form input[name=tel]').val(),
 			name: jQuery('.header__form input[name=name]').val(),
 			mail: jQuery('.header__form textarea[name=text]').val(),
 		}
 
-		);
+	);
 
 
 	jqXHR.done(function (responce) {
 		console.log(responce);
-		document.location.href = "../thank-you.html"; 
-		jQuery('.header__form input[name=tel]').val("");  
+		document.location.href = "../thank-you.html";
+		jQuery('.header__form input[name=tel]').val("");
 		jQuery('.header__form input[name=name]').val("");
 		jQuery('.header__form textarea[name=text]').val("");
 	});
